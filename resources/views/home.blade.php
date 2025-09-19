e03ef02ccde1b0e6effe26586d5ee035dfb657a2
RomanStar228 <@rbiu.local>
2025-09-19 09:33:16 +0500

    new-work

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <div class="logo-section">
+            <div class="logo-image">
+                <img src="" alt="">
+                <p>logo</p>
+            </div>
+            <div class="logo-nav">
+                <a href="">Главная</a>
+                <a href="">Главная</a>
+            </div>
+        </div>
+    </header>
+    <main class="main">
+        <div class="main-section">
+            
+        </div>
+    </main>
+    <footer class="footer">
+        <div class="footer-info">
+            <p>Старовойтова Анастасия Сергеевна</p>
+            <img src="" alt="">
+        </div>
+
+    </footer>
+</body>
+</html>
