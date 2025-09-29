0973c60c19d9f8420bff6efd80f04eee2f402e13
RomanStar228 <@rbiu.local>
2025-09-29 12:59:44 +0500

    work29

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,7 @@ Route::get('/', function () {
 Route::get('/mysite', [TestController::class,'mysite']) ->name('mysite');
 
 Route::get('/contacts',[TestController::class,'contacts']) ->name('contacts') ;
+
+Route::get('/products', [ProductController::class,'index']) -> name('products.index');
+
+Route::delete('/products/{product}', [ProductController::class, 'destroy']) -> name ('product.destroy');
